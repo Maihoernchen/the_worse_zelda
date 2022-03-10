e9bee747c1af5dd3e23f7f0365f8b990111a7f79
Kali Live user <maihoernchen.index@gmail.com>
2022-03-10 12:19:09 +0000

    duck

--- items.py
+++
@@ -1,4 +0,0 @@
-class light_leather_armor:
-    armor = 5
-    slow = 0
-
--- npcs.py
+++
@@ -1,7 +0,0 @@
-import player
-
-class mountain_villager_1:
-    look = "reccources/npcs/mountain_villager_1"
-    def talkto(self,line,item):
-        print(line)
-        player.inventory.add(item)
